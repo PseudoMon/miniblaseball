627ca6e917a71c0b040c2e0af2ebbe229d485b6d
Aliya N. A <aliya.nisa@yahoo.com>
2021-07-20 14:45:35 +0700

    New players I forgot to commit, minor fixes

--- src/components/sidebar/Aboutbox.svelte
+++ src/components/sidebar/Aboutbox.svelte
@@ -1,7 +1,7 @@
 <div class="aboutbox">
     <p>#MiniBlaseball is a series of minis by <a href="https://twitter.com/HetreaSky" target="_blank">HetreaSky</a>, depicting the many players in <a href="https://blaseball.com" target="_blank">Internet League Blaseball</a>.</p> 
         
-    <p>All designs are non-canon and created by the fan community. Team placements updated as of the end of Season 16.</p>
+    <p>All designs are non-canon and created by the fan community. Team placements are not currently updated. Check the wiki for each player's current status.</p>
 
     <p>More players are still being added! Follow <a href="https://twitter.com/HetreaSky" target="_blank">@HetreaSky</a> on Twitter or the <a href="https://twitter.com/hashtag/MiniBlaseball" target="_blank">#MiniBlaseball</a> hashtag for updates.</p>
 
